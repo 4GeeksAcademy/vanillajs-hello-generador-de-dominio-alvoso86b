fd8a1ecc7dc90bcbdb1e9c2a708d8a467b541550
Yoalvarado25 <manacima2019@gmail.com>
2025-07-26 21:47:18 +0000

    primer commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,17 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+ let pronoun = ['the ','your ', 'our ', 'his ', 'she '];
+ let adj = ['great ', 'big ', 'amazing ' ,'awesome '];
+ let noun = ['jogger ', 'racoon ', 'dog ', 'rabbit ', 'cat '];
+  
+  // generating the domains
+  for(let i = 0; i < pronoun.length; i++) {
+  for(let a = 0; a < adj.length; a++) {
+  for(let b = 0; b < noun.length; b++) {
+  console.log(pronoun[i] + adj[a] + noun[b]+'.com'); 
+      }
+    }
+  } 
+}
+
