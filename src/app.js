c2cb1d1f9fe89942481e7823e221341cab45525f
Yoalvarado25 <manacima2019@gmail.com>
2025-08-04 10:22:06 +0000

    tercer commit

--- src/app.js
+++ src/app.js
@@ -6,17 +6,15 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
- let pronoun = ['the ','your ', 'our ', 'his ', 'she '];
- let adj = ['great ', 'big ', 'amazing ' ,'awesome '];
- let noun = ['jogger ', 'racoon ', 'dog ', 'rabbit ', 'cat '];
+ let pronoun = ['the','your', 'our ', 'his', 'she'];
+ let adj = ['great', 'big', 'amazing' ,'awesome'];
+ let noun = ['jogger', 'racoon', 'dog', 'rabbit', 'cat'];
   
   // generating the domains
-  for(let i = 0; i < pronoun.length; i++) {
-  for(let a = 0; a < adj.length; a++) {
-  for(let b = 0; b < noun.length; b++) {
-  console.log(pronoun[i] + adj[a] + noun[b]+'.com'); 
-      }
-    }
-  } 
+  let rdm1 = Math.floor(Math.random() * pronoun.length);
+  let rdm2 = Math.floor(Math.random() * adj.length);
+  let rdm3 = Math.floor(Math.random() * noun.length);
+  
+  document.querySelector("#dominio").innerHTML = pronoun[rdm1] + adj[rdm2] + noun[rdm3] + ".com";
 }
 
